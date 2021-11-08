bf4fdd2db583826f4b85e590a3e7c73c463aa402
Andre <andrelopessan@hotmail.com>
2021-11-08 09:51:54 -0300

    Adicionado Tag php

--- index.php
+++ index.php
@@ -1,4 +1,6 @@
 ﻿<!DOCTYPE html>
+<?php echo 'se você deseja rodar código PHP dentro de documentos XHTML ou XML,
+                utilize essas tags'; ?>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
